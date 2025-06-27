010e2687104a851eea03b5621911f4d1912a39f3
Arjun Ramesh <arjunr2@andrew.cmu.edu>
2025-06-27 13:27:52 -0400

    Fix typo in `gen_data.sh` to include baseline

--- gen_data.sh
+++ gen_data.sh
@@ -5,5 +5,4 @@ mkdir -p data
 python3 manage.py violations -p data-raw/beanstalk
 # Generate data NPZ files from raw
 python3 manage.py dataset -p data-raw/beanstalk -o data/beanstalk
-python3 manage.py dataset -p data-raw/beanstalk -o data/beanstalk
-
+python3 manage.py dataset -p data-raw/baseline -o data/baseline
